cc4bacb7ae3adf85c38026f759817de4cc41f1a5
David Cediel <esdago18@gmail.com>
2025-04-09 20:15:27 +0200

    Improve validation of ImageUrlRequest dto

--- src/main/java/com/cordilleracoffee/product/infrastructure/dto/ImageUrlRequest.java
+++ src/main/java/com/cordilleracoffee/product/infrastructure/dto/ImageUrlRequest.java
@@ -1,7 +1,8 @@
 package com.cordilleracoffee.product.infrastructure.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 
-public record ImageUrlRequest(@Pattern(regexp = "^[a-zA-Z0-9_\\-.]+\\.(png|jpg)$",
+public record ImageUrlRequest(@NotBlank @Pattern(regexp = "^[a-zA-Z0-9_\\-.]+\\.(png|jpg)$",
         message = "Incorrect file type. Only .jpg and .png are allowed") String imageName) {
 }
