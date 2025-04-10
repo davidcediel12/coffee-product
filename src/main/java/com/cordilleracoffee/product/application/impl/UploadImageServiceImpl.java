ef894234c04894e9966a8703787e342d92615445
David Cediel <esdago18@gmail.com>
2025-04-10 18:21:48 +0200

    Use custom @UseCase annotation for application services to explicitly show its responsibility

--- src/main/java/com/cordilleracoffee/product/application/impl/UploadImageServiceImpl.java
+++ src/main/java/com/cordilleracoffee/product/application/impl/UploadImageServiceImpl.java
@@ -1,19 +1,19 @@
 package com.cordilleracoffee.product.application.impl;
 
+import com.cordilleracoffee.product.application.FileStorageRepository;
 import com.cordilleracoffee.product.application.UploadImageService;
+import com.cordilleracoffee.product.application.annotation.UseCase;
 import com.cordilleracoffee.product.application.exception.UnauthorizedUserException;
 import com.cordilleracoffee.product.domain.model.UserRole;
 import com.cordilleracoffee.product.infrastructure.dto.ImageUrlRequest;
 import com.cordilleracoffee.product.infrastructure.dto.ImageUrlRequests;
 import com.cordilleracoffee.product.infrastructure.dto.SignedUrl;
-import com.cordilleracoffee.product.application.FileStorageRepository;
-import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
-@Service
+@UseCase
 public class UploadImageServiceImpl implements UploadImageService {
 
     public static final String TEMP_FOLDER = "temp";
@@ -27,7 +27,7 @@ public class UploadImageServiceImpl implements UploadImageService {
     @Override
     public List<SignedUrl> getSignedUrls(ImageUrlRequests urlRequests, List<UserRole> userRoles) {
 
-        if(!userRoles.contains(UserRole.SELLER)){
+        if (!userRoles.contains(UserRole.SELLER)) {
             throw new UnauthorizedUserException("User must be seller to request url to upload images");
         }
 
