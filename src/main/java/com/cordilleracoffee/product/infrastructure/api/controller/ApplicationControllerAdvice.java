6171ae27daed762ab3f6b6e592e9fe6be4a3ce4c
David Cediel <esdago18@gmail.com>
2025-04-15 23:08:42 +0200

    Control more exception types in controller advice

--- src/main/java/com/cordilleracoffee/product/infrastructure/api/controller/ApplicationControllerAdvice.java
+++ src/main/java/com/cordilleracoffee/product/infrastructure/api/controller/ApplicationControllerAdvice.java
@@ -1,6 +1,7 @@
 package com.cordilleracoffee.product.infrastructure.api.controller;
 
 import com.cordilleracoffee.product.application.exception.UnauthorizedUserException;
+import com.cordilleracoffee.product.domain.exception.InvalidProductException;
 import com.cordilleracoffee.product.infrastructure.dto.ApiErrorResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -28,7 +29,7 @@ public class ApplicationControllerAdvice {
 
         return ResponseEntity.badRequest()
                 .body(new ApiErrorResponse(LocalDateTime.now(), "PRD-VA-01", errors,
-                        ServletUriComponentsBuilder.fromCurrentRequest().toUriString()));
+                        getUriString()));
     }
 
 
@@ -38,7 +39,7 @@ public class ApplicationControllerAdvice {
 
         return ResponseEntity.badRequest()
                 .body(new ApiErrorResponse(LocalDateTime.now(), "PRD-VA-02", error,
-                        ServletUriComponentsBuilder.fromCurrentRequest().toUriString()));
+                        getUriString()));
     }
 
 
@@ -47,13 +48,33 @@ public class ApplicationControllerAdvice {
 
         return ResponseEntity.badRequest()
                 .body(new ApiErrorResponse(LocalDateTime.now(), "PRD-VA-03", ex.getMessage(),
-                        ServletUriComponentsBuilder.fromCurrentRequest().toUriString()));
+                        getUriString()));
     }
 
     @ExceptionHandler(UnauthorizedUserException.class)
     ResponseEntity<ApiErrorResponse> handleUnauthorizedUserException(UnauthorizedUserException ex) {
         return ResponseEntity.status(HttpStatus.FORBIDDEN)
                 .body(new ApiErrorResponse(LocalDateTime.now(), "PRD-VA-04", ex.getMessage(),
-                        ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString()));
+                        getUriString()));
+    }
+
+    private static String getUriString() {
+        return ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString();
+    }
+
+
+    @ExceptionHandler(InvalidProductException.class)
+    ResponseEntity<ApiErrorResponse> handleInvalidProductException(InvalidProductException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ApiErrorResponse(LocalDateTime.now(), "PRD-VA-05", ex.getMessage(),
+                        getUriString()));
+    }
+
+    @ExceptionHandler
+    ResponseEntity<ApiErrorResponse> handleIllegalArgumentException(IllegalArgumentException ex) {
+        return ResponseEntity
+                .badRequest()
+                .body(new ApiErrorResponse(LocalDateTime.now(), "PRD-VA-06", ex.getMessage(), getUriString()));
     }
 }
