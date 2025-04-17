b2c5dd3eb32a433aefc50d3cbb0bd686a003f78e
David Cediel <esdago18@gmail.com>
2025-04-17 10:37:45 +0200

    Initialize category and types tables in tests

---
+++ src/test/resources/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO category(id, name, description) VALUES (1, 'appliance', 'product appliances');
+INSERT INTO tag_type (id, name, description) VALUES (1, 'machine type', 'type of machine');
+INSERT INTO tag(id, tag_type_id, name) VALUES (1, 1, 'electric machine');
